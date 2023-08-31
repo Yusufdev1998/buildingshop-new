f0eaf54089eb0e982c7f5652dbd9f24db5ecaf38
Yusufdev1998 <yusufdev1998@gmail.com>
2023-08-31 09:44:48 +0500

    all done

--- server/src/app.ts
+++ server/src/app.ts
@@ -1,5 +1,6 @@
 import express from "express";
 import cors from "cors";
+import helmet from "helmet";
 
 import v1Routes from "./routes/v1/v1Routes";
 
@@ -7,10 +8,10 @@ const app = express();
 
 // Middlewares
 app.use(cors());
+app.use(helmet());
 app.use(express.json());
 
 // API routes
-
 app.use("/api/v1", v1Routes);
 
 export default app;
